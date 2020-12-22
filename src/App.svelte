1b3257575ac83d05807ca4e63404334ac584a900
patrickeddy <p@patrickeddy.com>
2020-12-21 17:57:18 -1000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import firebase from "firebase";
+  import { FirebaseApp } from "sveltefire";
+  import { routes } from "../.routify/routes";
+
+  let firebaseConfig = {
+    // config goes here
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<FirebaseApp {firebase}>
+  <Router {routes} />
+</FirebaseApp>
